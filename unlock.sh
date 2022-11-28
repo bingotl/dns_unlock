496732abe148eb521c49b3a7c9e76462e92f9f54
bingotl <383201930@qq.com>
2022-11-28 20:29:08 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-#https://github.com/myxuchangbin/dnsmasq_sniproxy_install
-#https://raw.githubusercontent.com/myxuchangbin/dnsmasq_sniproxy_install/master/sniproxy.conf
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+red='\033[0;31m'
+green=[${green}OK${plain}] 
+yellow=[${yellow}Info${plain}] 
+red=[${red}Error${plain}] 
 
+unlock(){
 if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
     yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
     yum install bind-utils
@@ -20,16 +26,16 @@ else
 fi
 
 if [ $? -eq 0 ]; then
-    systemctl enable dnsmasq
+	systemctl enable dnsmasq
 	chattr -i /etc/resolv.conf
 	
-	if [ ! -f '/etc/resolv.conf' ];then
+	if [ ! -f '/etc/resolv.conf.bak' ];then
 		cp -f /etc/resolv.conf /etc/resolv.conf.bak
 	fi
 	rm -f /etc/resolv.conf
-    echo "nameserver 127.0.0.1" > /etc/resolv.conf
+	echo "nameserver 127.0.0.1" > /etc/resolv.conf
 	chattr +i /etc/resolv.conf
-cat > /etc/dnsmasq.d/unlock.conf <<EOF
+	cat > /etc/dnsmasq.d/unlock.conf <<EOF
 domain-needed
 bogus-priv
 no-resolv
@@ -148,16 +154,57 @@ address=/hinet.net/$1
 EOF
 
     systemctl restart dnsmasq
-    echo "dnsmasq启动成功"
+    echo -e "${green} dnsmasq启动成功"
     echo ""
-    echo "本机当前DNS"
+    echo -e "${yellow} 系统当前DNS（显示为127.0.0.1说明成功）"
     echo "---------------------"
     cat /etc/resolv.conf
     echo ""
-    echo "备份DNS/etc/resolv.conf.bak"
+    echo -e "${yellow} DNS备份文件 /etc/resolv.conf.bak"
     echo "---------------------"
     cat /etc/resolv.conf.bak
-    echo "建议把系统默认DNS添加到/etc/dnsmasq.d/unlock.conf文件中"
+    echo ""
+    echo -e "${yellow} 建议把系统默认DNS添加到/etc/dnsmasq.d/unlock.conf文件中"
 else
-    echo "dnsmasq安装失败, 请检查仓库状况"
+    echo -e "${red} dnsmasq安装失败, 请检查仓库状况"
 fi
+}
+
+re_dns(){
+if [ -f '/etc/resolv.conf.bak' ];then
+	echo -e "${yellow} 检测到DNS备份文件，从备份文件恢复系统DNS设置..."
+	chattr -i /etc/resolv.conf
+	rm -f /etc/resolv.conf
+	cp -f /etc/resolv.conf.bak /etc/resolv.conf
+else
+	echo -e "${yellow} 没有备份DNS文件，使用通用DNS"
+	chattr -i /etc/resolv.conf
+	cat > /etc/resolv.conf <<EOF
+nameserver 8.8.8.8
+nameserver 1.1.1.1
+EOF
+fi
+	echo -e "${yellow} 禁用dnsmasq服务..."
+	systemctl stop dnsmasq
+	systemctl disable dnsmasq
+	echo -e "${green} 完成，查看当前系统DNS（不是127.0.0.1说明成功）"
+	echo "---------------------"
+	cat /etc/resolv.conf
+}
+
+case "$1" in
+"")
+echo -e "${yellow} 当前系统DNS"
+echo "---------------------"
+cat /etc/resolv.conf
+;;
+
+"r")
+re_dns
+;;
+
+#else
+*)
+unlock $1
+;;
+esac
